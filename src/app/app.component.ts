1932d0c11cc34e72e7d03026743f6245a3e174f5
bbakercello <bbakercello@gmail.com>
2022-11-03 10:53:47 -0400

    styling added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+//component declaration
 @Component({
+  //this selector is being called in the main HTML body tag
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// methods go here
 export class AppComponent {
-  title = 'angular-practice';
+  title: string = 'Task Tracker';
 }
